e14bd67372c4b324c668a6421f51679dfd9ff8fe
Celal Küçükbulut <celalkucukbulut@gmail.com>
2022-02-24 19:45:35 +0300

    burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu'
+
+
 const Header = () => {
 
     return (
@@ -23,6 +26,27 @@ const Header = () => {
                 <NavLink className="navlink" to='/ContactUs' activestyle="true">
                     İLETİŞİM
                 </NavLink>
+            </div>
+            <div className='navburger'>
+                <Menu right width={250} >
+                    <div className='navmenu'>
+                        <NavLink className="navlink" to='/Home' activestyle="true">
+                            ANASAYFA
+                        </NavLink>
+                        <NavLink className="navlink" to='/AboutUs' activestyle="true">
+                            HAKKIMIZDA
+                        </NavLink>
+                        <NavLink className="navlink" to='/Fanila' activestyle="true">
+                            KASTAMONU FANİLASI
+                        </NavLink>
+                        <NavLink className="navlink" to='/Products' activestyle="true">
+                            ÜRÜNLERİMİZ
+                        </NavLink>
+                        <NavLink className="navlink" to='/ContactUs' activestyle="true">
+                            İLETİŞİM
+                        </NavLink>
+                    </div>
+                </Menu>
             </div>
         </nav>
     )
