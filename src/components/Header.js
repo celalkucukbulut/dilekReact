b8903615190404ef697fd38ecd0983405647015b
Celal Küçükbulut <celalkucukbulut@gmail.com>
2022-02-26 21:58:42 +0300

    outside click handler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React,{ useState }  from 'react'
 import { NavLink } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu'
-
+import OutsideArea from './OutsideArea';
 
 const Header = () => {
     const [isMenuOpen, handleMenu] = useState(false);
     const handleCloseMenu = () => {
-      handleMenu(false);
+        handleMenu(false);
     };
-    
     const handleStateChange = (state) => {
-      handleMenu(state.isOpen);
+        handleMenu(state.isOpen);
     };
+
     return (
         <nav className='nav'>
             <NavLink className="navlink" to='/'>
@@ -34,32 +34,34 @@ const Header = () => {
                     İLETİŞİM
                 </NavLink>
             </div>
-            <div className='navburger'>
-                <Menu 
-                    right 
-                    width={250} 
-                    isOpen={isMenuOpen}
-                    onStateChange={handleStateChange}
-                >
-                    <div className='navmenu'>
-                        <NavLink className="navlink" to='/Home' activestyle="true" onClick={() => handleCloseMenu()}>
-                            ANASAYFA
-                        </NavLink>
-                        <NavLink className="navlink" to='/AboutUs' activestyle="true" onClick={() => handleCloseMenu()}>
-                            HAKKIMIZDA
-                        </NavLink>
-                        <NavLink className="navlink" to='/Fanila' activestyle="true" onClick={() => handleCloseMenu()}>
-                            KASTAMONU FANİLASI
-                        </NavLink>
-                        <NavLink className="navlink" to='/Products' activestyle="true" onClick={() => handleCloseMenu()}>
-                            ÜRÜNLERİMİZ
-                        </NavLink>
-                        <NavLink className="navlink" to='/ContactUs' activestyle="true" onClick={() => handleCloseMenu()}>
-                            İLETİŞİM
-                        </NavLink>
-                    </div>
-                </Menu>
-            </div>
+            <OutsideArea runFunction={() => handleCloseMenu()}>
+                <div className='navburger'>
+                    <Menu
+                        right
+                        width={250}
+                        isOpen={isMenuOpen}
+                        onStateChange={handleStateChange}
+                    >
+                        <div className='navmenu'>
+                            <NavLink className="navlink" to='/Home' activestyle="true" onClick={() => handleCloseMenu()}>
+                                ANASAYFA
+                            </NavLink>
+                            <NavLink className="navlink" to='/AboutUs' activestyle="true" onClick={() => handleCloseMenu()}>
+                                HAKKIMIZDA
+                            </NavLink>
+                            <NavLink className="navlink" to='/Fanila' activestyle="true" onClick={() => handleCloseMenu()}>
+                                KASTAMONU FANİLASI
+                            </NavLink>
+                            <NavLink className="navlink" to='/Products' activestyle="true" onClick={() => handleCloseMenu()}>
+                                ÜRÜNLERİMİZ
+                            </NavLink>
+                            <NavLink className="navlink" to='/ContactUs' activestyle="true" onClick={() => handleCloseMenu()}>
+                                İLETİŞİM
+                            </NavLink>
+                        </div>
+                    </Menu>
+                </div>
+            </OutsideArea>
         </nav>
     )
 };
--- src/components/ProductImage.js
+++ src/components/ProductImage.js
@@ -1,31 +1,33 @@
 
 import React, {useState} from 'react'
+import OutsideArea from './OutsideArea';
 
 const ProductImage = (props) => {
-    const [isOpen,setOpen] = useState(false);
-    console.log(isOpen)
+    const [isOpen, setOpen] = useState(false);
+
     return (
         <div className='productImage'>
-            <img 
-                className='img' 
+            <img
+                className='img'
                 onClick={() => setOpen(!isOpen)}
-                alt='dilek_fanila' 
+                alt='dilek_fanila'
                 src={props.url} />
             <label className='productImageLabel'>{props.label}</label>
             {isOpen && (
-                <dialog
-                  className="dialogProduct"
-                  open
-                  onClick={() => setOpen(!isOpen)}
-                >
-                  <img 
-                    className='dialogImg' 
-                    onClick={() => setOpen(!isOpen)}
-                    alt='dilek_fanila' 
-                    src={props.url}
-                  />
-                </dialog>
-              )}
+                <OutsideArea runFunction={() => setOpen(!isOpen)}>
+                    <dialog
+                        className="dialogProduct"
+                        onClick={() => setOpen(!isOpen)}
+                    >
+                        <img
+                            className='dialogImg'
+                            onClick={() => setOpen(!isOpen)}
+                            alt='dilek_fanila'
+                            src={props.url}
+                        />
+                    </dialog>
+                </OutsideArea>
+            )}
         </div>
     )
 }
