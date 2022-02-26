ddcb49ba223dfe6ac60c4cec5d945cd02d331feb
Celal Küçükbulut <celalkucukbulut@gmail.com>
2022-02-26 21:59:23 +0300

    OutsideArea component

---
+++ src/components/OutsideArea.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react'
+
+const useOutside = (ref,runFunction) => {
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                runFunction();
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
+}
+
+const OutsideArea = (props) => {
+    const wrapperRef = useRef(null);
+    useOutside(wrapperRef,props.runFunction);
+
+    return <div ref={wrapperRef}>{props.children}</div>;
+}
+export default OutsideArea
