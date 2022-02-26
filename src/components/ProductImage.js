29848c958337a970683cecaba036139b06c4fe06
Celal Küçükbulut <celalkucukbulut@gmail.com>
2022-02-26 15:56:18 +0300

    image dialog added

--- src/components/ProductImage.js
+++ src/components/ProductImage.js
@@ -1,10 +1,31 @@
-import React from 'react'
+
+import React, {useState} from 'react'
 
 const ProductImage = (props) => {
+    const [isOpen,setOpen] = useState(false);
+    console.log(isOpen)
     return (
         <div className='productImage'>
-            <img className='img' alt='dilek_fanila' src={props.url}></img>
+            <img 
+                className='img' 
+                onClick={() => setOpen(!isOpen)}
+                alt='dilek_fanila' 
+                src={props.url} />
             <label className='productImageLabel'>{props.label}</label>
+            {isOpen && (
+                <dialog
+                  className="dialogProduct"
+                  open
+                  onClick={() => setOpen(!isOpen)}
+                >
+                  <img 
+                    className='dialogImg' 
+                    onClick={() => setOpen(!isOpen)}
+                    alt='dilek_fanila' 
+                    src={props.url}
+                  />
+                </dialog>
+              )}
         </div>
     )
 }
