217dfccc552bacf5698e3c7fb3d76f38babdb1eb
Celal Küçükbulut <celalkucukbulut@gmail.com>
2022-10-29 16:56:13 +0300

    firebase cli project added

--- src/components/Text.js
+++ src/components/Text.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Text = (props) => {
   return (
     <div>
-        {props.subtitle ? <h2 className="subtitle">{props.subtitle}</h2> : null }
+        {props.subtitle ? <h1 className="subtitle">{props.subtitle}</h1> : null }
         {props.text ? <p className="text">{props.text}</p> : null }
     </div>
   )
