4703f45e031c3d2a7e525851ab2763caf0b231b8
Celal Küçükbulut <celalkucukbulut@gmail.com>
2025-01-06 14:51:43 +0300

    whatsapp url added

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,6 +6,7 @@ import Text from './Utils/Text';
 //#region import images
 
 import instagramImage from '../Images/instagram.png';
+import whatsappImage from '../Images/whatsapp.png';
 
 //#endregion
 const Introduction = () => {
@@ -14,6 +15,9 @@ const Introduction = () => {
     const [introductionText, setIntroductionText] = useState('')
     const [instagram, setInstagram] = useState('')
     const [instagramText, setInstagramText] = useState('')
+    const [whatsapp, setWhatsapp] = useState('')
+    const [whatsappTemplate, setWhatsappTemplate] = useState('')
+    const [whatsappText, setWhatsappText] = useState('')
 
     useEffect(() =>
         onSnapshot(doc(db, "dilekdb", "introduction"), (doc) => {
@@ -25,13 +29,25 @@ const Introduction = () => {
             setInstagram(doc.data().instagram)
             setInstagramText(doc.data().instagramText)
         }), [])
+    useEffect(() =>
+        onSnapshot(doc(db, "dilekdb", "contactUs"), (doc) => {
+            setWhatsapp(doc.data().whatsapp)
+            setWhatsappTemplate(doc.data().whatsappTemplate)
+            setWhatsappText(doc.data().whatsappText)
+        }), [])
     return (
         <div>
             {introductionTitle || introductionText ?
                 <div className='content'>
                     <Text subtitle={introductionTitle} text={introductionText}></Text>
+                    {whatsapp ? <div>
+                        <a className="link" rel="noopener noreferrer" target="_blank" href={whatsapp+encodeURIComponent(whatsappTemplate)}><img className='iconImage'
+                            alt='whatsapp'
+                            src={whatsappImage}
+                        /> {whatsappText} </a></div> : null
+                    }
                     {instagram ? <div>
-                        <a className="link" rel="noopener noreferrer" target="_blank" href={instagram}><img className='instagramImage'
+                        <a className="link" rel="noopener noreferrer" target="_blank" href={instagram}><img className='iconImage'
                             alt='instagram'
                             src={instagramImage}
                         /> {instagramText} </a></div> : null
